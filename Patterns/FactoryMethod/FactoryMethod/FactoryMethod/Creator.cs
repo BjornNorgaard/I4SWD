65e62b22ab3813e6852582af1fc7b79e4f4bd763
Bjørn Nørgaard <Bjornsnorgaard@gmail.com>
2016-01-03 10:34:06 +0100

    fac method example, done need to email kasper some questions

---
+++ Patterns/FactoryMethod/FactoryMethod/FactoryMethod/Creator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public abstract class Creator
+    {
+        // IProduct to be used for stuff
+        IProduct _myProduct;
+
+        // ctor eller anden nyttig function?
+        // public Creator()
+        public void DoSomething()
+        {
+            _myProduct = MakeProduct();
+            Console.WriteLine(_myProduct.GetType().Name + " says hello!");
+        }
+
+        // Factory method -> to be implemented in subclass...
+        public abstract IProduct MakeProduct();
+    }
+
+    public class ConcreteCreator : Creator
+    {
+        public override IProduct MakeProduct()
+        {
+            // returns some subclass of IProduct
+            return new ConcreteProduct();
+        }
+    }
+}
